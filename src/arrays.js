5d3ae6888227c62b0a26563aa49f2700cf0a0225
Luka Popovic <lukapopovic0111@gmail.com>
2025-11-24 12:37:36 +0100

    Add difficulty and category arrays

---
+++ src/arrays.js
@@ -0,0 +1,11 @@
+export const difficultyArray = [
+  { id: 1, value: "easy" },
+  { id: 2, value: "medium" },
+  { id: 3, value: "hard" },
+];
+export const categoryArray = [
+  { id: 1, value: "sports" },
+  { id: 2, value: "art" },
+  { id: 3, value: "geography" },
+  { id: 4, value: "mythology" },
+];
